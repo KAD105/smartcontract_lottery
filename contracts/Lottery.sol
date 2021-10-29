75c08b4dbe5b9e2b24d21dd495876e8eebe8b54c
kazem <kazem_abdi@yahoo.com>
2021-10-28 17:18:07 -0700

    update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,53 +1,56 @@
-
-
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
+contract Lottery is VRFConsumerBase, Ownable {
+    address payable[] public palyers;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-contract Lottery is Ownable{
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSE,
+        CALCULATING_STATE
+    }
+    // LOTTERY_STATE is a type.
+    // OPEN, CLOSE, CALCULATING_STATE = 0,1,2
 
-   address payable[] public  palyers;
-   uint256 public usdEntryFee;
-   AggregatorV3Interface internal ethUsdPriceFeed;
+    LOTTERY_STATE public lottery_state;
 
-   enum LOTTERY_STATE{
-       OPEN,
-       CLOSE,
-       CALCULATING_STATE
-   }
-   // LOTTERY_STATE is a type. 
-   // OPEN, CLOSE, CALCULATING_STATE = 0,1,2
-
-   LOTTERY_STATE public lottery_state;
-
-    constructor(address _priceFeedAddress) public{
+    constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSE;
         // We can also set it close like this: lottery_state = 1
-       
     }
-    function enter() public payable{
+
+    function enter() public payable {
         // 50$ minimum
         require(msg.value >= getEnteranceFee(), "Not enough Eth!");
         require(lottery_state == LOTTERY_STATE.OPEN);
         palyers.push(msg.sender);
-       
     }
-    function getEnteranceFee() public view returns(uint256){
+
+    function getEnteranceFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
         uint256 adjustedPrice = uint256(price) * 10**10; //18 decimals
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
+
     function startLottery() public onlyOwner {
-        require(lottery_state == LOTTERY_STATE.CLOSE, "Can't start a lottery yet!");
+        require(
+            lottery_state == LOTTERY_STATE.CLOSE,
+            "Can't start a lottery yet!"
+        );
         require(lottery_state == LOTTERY_STATE.OPEN);
     }
-    function endLottery() public{}
-}
 
+    function endLottery() public onlyOwner {
+        lottery_state = LOTTERY_STATE.CALCULATING_STATE;
+    }
+}
