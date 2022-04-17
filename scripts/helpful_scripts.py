0da122228c9d8a198fccaf2698742e5dbbc99c76
KAD105 <kazem_abdi@yahoo.com>
2022-04-17 14:10:45 +0200

    chancged helpful_scripts.py

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,10 +1,19 @@
+from pickle import NONE
 from brownie import network, accounts, config
 
 LOCAL_BLOCKCHAIN_ENVIRONMETN = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRIONMENT = ["mainnet-fork", "mainnet-fork-dev"]
 
 
-def get_account():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMETN:
+def get_account(index=None, id=None):
+
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMETN
+        or network.show_active() in FORKED_LOCAL_ENVIRIONMENT
+    ):
         return accounts[0]
-    else:
-        return accounts.add(config["wallets"]["from_key"])
+    return accounts.add(config["wallets"]["from_key"])
